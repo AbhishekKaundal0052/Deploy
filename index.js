032d1c290dab968f1d1ec8855c61ed7dacf794cb
abhishekkaundal0052 <kaundal.abhishek0052@gmail.com>
2024-02-22 10:47:49 +0530

    deployed

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 const port = 8080;
+app.get('/',(req, res)=>{
+    res.send('Deployed');
+});
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
   })
